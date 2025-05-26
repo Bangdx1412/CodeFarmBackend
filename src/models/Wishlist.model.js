3b1fab9c2d0bf87e6585890b56116f691122815c
Vương Sơn <admin@VuongSon-2.local>
2025-05-27 00:02:11 +0700

    wishlist

--- src/models/Wishlist.model.js
+++ src/models/Wishlist.model.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 const wishlistSchema = new mongoose.Schema({
   user_id: String,
-  product_id: String,
-  addedAt: Date,
+  products: [
+    {
+      product_id: String,
+      addedAt: Date,
+    }
+  ],
 }, { versionKey: false });
 
 const Wishlist = mongoose.model("Wishlist", wishlistSchema, "wishlists");
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import productRoute  from "./product.route.js";
 import categoryRoute  from "./category.route.js";
 import authRoute  from "./auth.route.js";
+import wishlistRoute from "./wishlist.route.js";
 const routes = Router();
 
 routes.use("/products", productRoute)
 routes.use("/categories", categoryRoute)
 routes.use("/auth", authRoute)
+routes.use("/wishlist", wishlistRoute);
 // routes.use("/products", hanldeProduct...)
 // routes.use("/products", hanldeProduct...)
 // routes.use("/products", hanldeProduct...)
